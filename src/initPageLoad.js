ebb5a6100b88c113b0c012d3ba716a15d78d386f
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-02-10 07:26:48 +0100

    Fix name issues
    Fix some naming issues where elements from different files shared the same class name;
    Fixed it by giving more detailed name;

--- src/initPageLoad.js
+++ src/initPageLoad.js
@@ -1,23 +1,32 @@
 export default initLoad;
-import Troja from "./images/restaurant-troja.jpg";
+import Troja from "./images/restaurant-troja-door.jpg";
 
 const divContent = document.getElementById("content");
-const headline = document.createElement("h2");
+const container = document.createElement("div");
+const headline = document.createElement("h1");
 const imgPlaceholder = document.createElement("div");
-const subText = document.createElement("div");
+const subText = document.createElement("h2");
 const image = new Image();
+const body = document.body;
 
 function initLoad() {
-    headline.textContent = "This is Troy!"
-    headline.classList.add("headline");
-    divContent.appendChild(headline);
+    container.classList.add("init-content-container");
 
+    headline.textContent = "This is Troja!"
+    headline.classList.add("init-headline");
+    container.appendChild(headline);
+
+    /*
     image.src = Troja;
     imgPlaceholder.appendChild(image);
     imgPlaceholder.classList.add("placeholder");
     divContent.appendChild(imgPlaceholder);
+    */
 
-    subText.textContent = "You may have heard about us from anchient sources, but we promise: This is WAY better!"
+    subText.textContent = "Myth, Legend, Restaurant."
     subText.classList.add("sub-text")
-    divContent.appendChild(subText);
+    container.appendChild(subText);
+
+    divContent.appendChild(container)
+    body.classList.add("background-init");
 }
