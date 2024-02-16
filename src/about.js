99cdf7b6c6932a652e5b7cbf81759aa2c8fecff5
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-02-16 17:14:02 +0100

    Update about.js
    add Paragraph class that create a paragraph for text input;
    this way the about page is not just one big text block but will be structured into smaller paragraph sections, providing a better structure and UX;

--- src/about.js
+++ src/about.js
@@ -2,14 +2,39 @@ export default displayAbout;
 
 const divContent = document.getElementById("content");
 const headline = document.createElement("h1");
-const story = document.createElement("div");
+const disclaimer = document.createElement("div");
+
+class Paragraph {
+    constructor(content) {
+        this.content = content;
+    }
+
+    create() {
+        let paragraph = document.createElement("p");
+        paragraph.classList.add("about-paragraph");
+        paragraph.textContent = `${this.content}`;
+
+        disclaimer.appendChild(paragraph);
+    }
+}
 
 function displayAbout() {
-    headline.textContent = "About Troja";
+    headline.textContent = "Disclaimer";
     headline.classList.add("about-headline");
     divContent.appendChild(headline);
 
-    story.textContent = "Welcome to Troja, where culinary excellence meets ancient allure! Troja is more than just a restaurant—it's a gastronomic odyssey that transcends time and taste.Inspired by the epic tales of the legendary city of Troy, our restaurant is a celebration of flavors that have withstood the test of centuries. Drawing inspiration from the rich tapestry of ancient civilizations, we bring you a dining experience that blends tradition with modern sophistication. Embark on a culinary journey as we transport you to a world where each dish tells a story, echoing the resilience and splendor of the ancient city itself. From the moment you step through our doors, you are greeted by an ambiance that mirrors the grandeur of the mythical Troy, creating an immersive dining experience. Our chefs, like skilled artisans, craft each dish with passion and precision, using only the finest ingredients sourced from local farmers and purveyors. The menu, a homage to the diverse flavors of the Mediterranean, invites you to savor every bite as if discovering a hidden treasure. At Troy, we believe in the power of shared moments and communal dining. Our inviting atmosphere is designed to encourage connections and conversations, much like the camaraderie that defined the ancient city's legendary walls. Whether you're gathering with friends, family, or colleagues, Troja is the perfect setting for creating lasting memories.Just as the heroes of old embarked on epic quests, our commitment is to provide you with an extraordinary dining experience that transcends expectations. Join us at Troja, where history, hospitality, and flavor converge to create a tale as timeless as the city that inspired it.";
-    story.classList.add("story");
-    divContent.appendChild(story);
-}
+    disclaimer.classList.add("disclaimer-container");
+    divContent.appendChild(disclaimer);
+
+    let p1 = new Paragraph('This website is a project developed by "The Odin Project" for educational purposes only. It is not a real website, and any resemblance to actual businesses or individuals is purely coincidental.');
+    p1.create();
+
+    let p2 = new Paragraph('The content and design of this website were inspired by "Troja - im Grünen Kranz," a Greek restaurant located in Munich-Laim. The photos used on this site are sourced from the mentioned restaurant. We acknowledge and appreciate the real restaurant for its fabulous food and beautiful ambiance, and we encourage visitors to experience it firsthand. This website does not aim to impersonate or replicate the real business, and its content is not representative of the actual offerings or services of "Troja - im Grünen Kranz."');
+    p2.create();
+
+    let p3 = new Paragraph('For accurate information about the restaurant, its menu, and services, please visit the official website or contact "Troja - im Grünen Kranz" directly.');
+    p3.create();
+
+    let p4 = new Paragraph('Thank you for understanding the educational nature of this project and respecting the authenticity of the real establishment.');
+    p4.create();
+};
