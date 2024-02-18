44f13c1b944e5f11f1c44ea2f30e828c42490ced
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-02-18 21:08:00 +0100

    fix typo in text-content

--- src/about.js
+++ src/about.js
@@ -25,7 +25,7 @@ function displayAbout() {
         }
     };
 
-    let p1 = new Paragraph('This website is a project developed by "The Odin Project" for educational purposes only. It is not a real website, and any resemblance to actual businesses or individuals is purely coincidental.');
+    let p1 = new Paragraph('This website is a project developed as part of "The Odin Project" for educational purposes only and is not a real website.');
     let p2 = new Paragraph('The content and design of this website were inspired by "Troja - im Grünen Kranz," a Greek restaurant located in Munich-Laim. The photos used on this site are sourced from the mentioned restaurant. We acknowledge and appreciate the real restaurant for its fabulous food and beautiful ambiance, and we encourage visitors to experience it firsthand. This website does not aim to impersonate or replicate the real business, and its content is not representative of the actual offerings or services of "Troja - im Grünen Kranz."');
     let p3 = new Paragraph('For accurate information about the restaurant, its menu, and services, please visit the official website or contact "Troja - im Grünen Kranz" directly.');
     let p4 = new Paragraph('Thank you for understanding the educational nature of this project and respecting the authenticity of the real establishment.');
